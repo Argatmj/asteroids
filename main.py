25ed57d5c958c4a3f6ef360d10240cf4d92a05dc
Argatmj <ikomangarga0612@gmail.com>
2025-02-04 10:33:22 +0100

    Added player movement

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame
 from constants import *
+from player import * 
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+    p1 = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         for event in pygame.event.get():
@@ -10,8 +15,11 @@ def main():
                 return
         
         screen.fill((0,0,0))
+        p1.update(dt)
+        p1.draw(screen)
 
         pygame.display.flip()
+        dt = time.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
